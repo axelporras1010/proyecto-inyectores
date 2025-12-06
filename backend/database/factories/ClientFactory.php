3dc1f090c4bbb3cbe6d7d4ce4b4d556285c40903
Axel Porras <axelporrasg@gmail.com>
2025-12-06 16:16:41 -0400

    draft of seeders

---
+++ backend/database/factories/ClientFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class ClientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'phone' => fake()->phoneNumber(),
+            'cedula' => fake()->randomNumber(9, true)
+        ];
+    }
+}
